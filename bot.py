148871d1ec373bbdbe1287013dd7c58170403c0d
KING BALA <100426391+Sunmusicking@users.noreply.github.com>
2022-03-17 13:21:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -21,26 +21,26 @@ spam_chats = []
 @client.on(events.NewMessage(pattern="^/start$"))
 async def start(event):
   await event.reply(
-    "__**I'm MentionAll Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github",
+    "__**I'm KingMention Bot**, I can mention almost all members in group or channel 👻\nClick **/help** for more information__\n\n Follow [@KING_0F_CB](https://github.com/Sunmusicking/MentionAllBot/edit/main/bot.py) on Github",
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('⭕ 𝗚𝗥𝗢𝗨𝗣', 'https://t.me/TAMIL_CHATBOX'),
+        Button.url('👉 𝗥𝗘𝗣𝗢', 'https://t.me/KING_0F_CB')
       ]
     )
   )
 
 @client.on(events.NewMessage(pattern="^/help$"))
 async def help(event):
-  helptext = "**Help Menu of MentionAllBot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@AnjanaMadu](https://github.com/AnjanaMadu) on Github"
+  helptext = "**Help Menu of KingMentionABot**\n\nCommand: /mentionall\n__You can use this command with text what you want to mention others.__\n`Example: /mentionall Good Morning!`\n__You can you this command as a reply to any message. Bot will tag users to that replied messsage__.\n\nFollow [@KING_0F_CB](https://github.com/Sunmusicking/MentionAllBot/edit/main/bot.py) on Github"
   await event.reply(
     helptext,
     link_preview=False,
     buttons=(
       [
-        Button.url('📣 Channel', 'https://t.me/harp_tech'),
-        Button.url('📦 Source', 'https://github.com/AnjanaMadu/MentionAllBot')
+        Button.url('⭕ 𝗚𝗥𝗢𝗨𝗣', 'https://t.me/TAMIL_CHATBOX'),
+        Button.url('👉 𝗥𝗘𝗣𝗢', 'https://t.me/KING_0F_CB')
       ]
     )
   )
